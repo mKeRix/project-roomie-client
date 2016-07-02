24c296bd1f99034d74bd6670abaa9d294302411f
Heiko Rothe <heikorothe@msn.com>
2016-07-02 13:54:55 +0200

    Initial commit

---
+++ public/assets/globals/js/screensaver.js
@@ -0,0 +1,27 @@
+$(function() {
+    // bind idle timer to document
+    $.idleTimer(30000);
+
+    // screensaver popup
+    $('#overlay-screensaver').popup({
+        color: 'black',
+        opacity: 1,
+        transition: '0.3s',
+        scrolllock: true
+    });
+
+    // flipclock
+    $('.flip-clock').FlipClock({
+        clockFace: 'TwentyFourHourClock'
+    });
+
+    $(document).on('idle.idleTimer', function (event, elem, obj) {
+        $('#overlay-screensaver').popup('show')
+    });
+    
+    $(document).on('active.idleTimer', function (event, elem, obj, triggerevent) {
+        $('#overlay-screensaver').popup('hide')
+    });
+
+
+});
