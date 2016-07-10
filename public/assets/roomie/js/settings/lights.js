9f767b47319bd10f3f9433e10e81f04d425c1ba8
Heiko Rothe <heikorothe@msn.com>
2016-07-10 23:30:48 +0200

    started UI work for bridge push link

--- public/assets/roomie/js/settings/lights.js
+++ public/assets/roomie/js/settings/lights.js
@@ -27,7 +27,11 @@ socket.on('bridges found', function(msg){
     msg.forEach(function (bridge) {
         list.append(
             $('<li>').attr('class', 'has-action-right').append(
-                $('<a>').attr('href', '#').attr('class', 'visible').append(
+                $('<a>').attr('href', '#')
+                    .attr('data-toggle', 'modal')
+                    .attr('data-target', '#push-link-modal')
+                    .attr('data-hostname', bridge.ipaddress)
+                    .attr('class', 'visible').append(
                     $('<div>').attr('class', 'list-content').append(
                         $('<span>').attr('class', 'title').append(bridge.ipaddress),
                         $('<span>').attr('class', 'caption').append('ID: ' + bridge.id)
@@ -39,4 +43,27 @@ socket.on('bridges found', function(msg){
             )
         )
     });
+});
+
+// carry ip to our modal
+$('#push-link-modal').on('show.bs.modal', function (event) {
+    // link that trigger the modal
+    var link = $(event.relatedTarget);
+    // get the associated IP
+    var hostname = link.data('hostname');
+    // grab the connect button and save the ip an anotehr attribute
+    var modal = $(this);
+    modal.find('#push-link-connect').attr('data-hostname', hostname);
+});
+
+// if connect button is clicked send request to server
+$('#push-link-connect').click(function () {
+    // grab the data from the button
+    var button = $(this),
+        par = button.parents('.modal-dialog:first');
+
+    socket.emit('connect to bridge', button.data('hostname'));
+
+    // visual cues
+    button.attr('disabled', true);
 });
